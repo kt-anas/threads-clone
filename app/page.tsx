fa89d2144e1024fe404c9588125331fbbbdf6fcf
anas <ktmanas2020@gmail.com>
2024-08-29 18:11:52 +0530

    home page sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
  
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
         <Navbar/>
-      <div>home</div>
+      
     </main>
   );
 }
